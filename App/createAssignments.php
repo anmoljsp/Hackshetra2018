5e1ba831dda8887ee36b625d5e9a3d7ec84b0653
Abhinandan <abhinandan578@gmail.com>
2018-01-14 01:25:43 +0530

    Working on assignmnets

---
+++ App/createAssignments.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Assignments</title>
+</head>
+<body>
+	<form action="fillAssignments.php" method ="POST">
+    <fieldset>
+      <legend>Assignments</legend>
+      <table>
+        <tr>
+          <td>Class</td>
+          <td><input type="text" name ="Class"></td>
+        </tr>
+        <tr>
+          <td>Subject</td>
+          <td><input type="text" name="Subject" ></td>
+        </tr>
+        <tr>
+          <td>Upload</td>
+          <td><input type="file" name="Subject" ></td>
+        </tr>
+        <tr>
+         <td colspan="2"><input type="submit" value="SubmiT" name="submit"></td>
+        </tr>
+      </table>
+    </fieldset>
+  </form>
+</body>
+</html>
---
+++ App/fillAssignments.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+include("config.php");
+include("encryption.php");	
+function redirect($url,$permanent=false)
+{
+	if($permanent)
+	{
+		header('HTTP/1.1 301 Moved Permanently');
+	}
+	header('Location: '.$url);
+	exit();
+}
+// if(!isset($_SESSION['Username']){
+// 	redirect("index.php");
+// }
+	$class= $subject= "";
+	$Username=$_SESSION['Username'];
+	echo "hey";
+	if($_SERVER["REQUEST_METHOD"]=="POST")
+	{
+		if(isset($_POST['submit']))
+		{
+			$class=$_POST["Class"];
+			$subject=$_POST["Subject"];
+			echo "Here it is";
+			if($class != ""&&$subject != "")
+			{
+		    	$query1="SELECT `Class_Id` FROM `Classes` WHERE `Class_Name`='".$class."'";
+		    	$res1=mysqli_query($db,$query);
+		    	$res1=mysqli_fetch_assoc($res1);
+		    	$class=$res1["Class_Id"];
+		    	$query2="SELECT `Subject_Id` FROM `Subjects` WHERE `Subject_Name`='".$subject."'";
+		    	$res2=mysqli_query($db,$query2);
+		    	$res2=mysqli_fetch_assoc($res2);
+		    	$subject=$res2["Subject_Id"];
+		    	$query3="SELECT `S.No.` FROM `Acounts` WHERE `Username`='".$Username."'";
+	      		$res3=mysqli_query($db,$query3);
+	      		$res3=mysqli_fetch_assoc($res3);
+	      		$T_Id=$res3["S.No."];
+		    	$query4="INSERT INTO `Assignments`(`Class_Id`, `Subject_Code`, `T_No`) VALUES ('$class','$subject','$T_Id')";
+		    	$res4=mysqli_query($db,$query4);
+		    	if($res4)
+		    	{
+		      		redirect('UserHome.html');
+		      		// echo $password;
+
+		    	}
+	    		else
+	    		{
+	    			echo "Error";
+	    			echo("Error description: " . mysqli_error($db));
+	    		}
+
+			}
+
+		}
+
+	}
+?>
