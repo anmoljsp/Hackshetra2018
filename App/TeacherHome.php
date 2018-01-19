e72be50a4c63e91a50c39f0e2c3af8d38a7e37f8
anmoljsp <anmoljsp@gmail.com>
2018-01-19 16:16:21 +0530

    final

--- App/TeacherHome.php
+++ App/TeacherHome.php
@@ -52,18 +52,16 @@ input{
   <nav class="navbar navbar-default fixed">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">Anonymous</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
-      <li><a href="#">Page 1</a></li>
-      <li><a href="#">Page 2</a></li>
-      <li><a href="#">Page 3</a></li>
+      <li><a href="../Editor/NicEdit.html">New Assignment</a></li>
+      <li><a href="logout.php">Logout</a></li>
     </ul>
   </div>
 </nav>
 <div  class="container" style="margin:0px;padding:0px;width:100%;min-height:100%;">
-  <h2 style="text-align: center;font-size:300%;">Anonymous</h2>
   <br><br><br><br><br>
   <div class="row" style="min-height:100%vh;">
     <div class="col-sm-3">
